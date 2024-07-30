78b6881789c330936810f404e35b42caa8f232ed
fagner ferreira <fagnerrc12@gmail.com>
2024-07-30 20:13:56 -0300

    update

--- client.gd
+++ client.gd
@@ -47,11 +47,11 @@ func _on_button_join_host():
 	var code: String = $"%iptext".text
 	if code.length() < 4:
 		return
-	var dest = ".".join(ip.split(".").slice(0,2)) + "." \
-				+ str(code.substr(0, 2).hex_to_int()) + "." \
-				+ str(code.substr(2, 3).hex_to_int())
+	var dest = ".".join(ip.split(".").slice(0, 2))+"."+\
+		".".join([0, 2].map(func(num): return str(code.substr(num, 2).hex_to_int())))
+	
 	$"textt".text += dest+"\n"
-	client.connect_to_host(dest, port)
+	print("connnnn",dest," ",client.connect_to_host(dest, port))
 	client.put_packet(("add,"+playername).to_utf8_buffer())
 
 func _on_button_create_host():
@@ -60,13 +60,18 @@ func _on_button_create_host():
 	
 	if udp_server.listen(port, ip) != OK:
 		print("erro ao escutar na porta ", port)
+	
+	var res = "".join(Array(ip.split(".").slice(2, 6)).map(func(num): return ("%x" % int(num)).lpad(2, "0"))).to_upper()
+	print(res)
 
 func  get_ip():
 	var ip: String
-	
-	for address in IP.get_local_addresses():
-		if (address.split('.').size() == 4):
-			ip=address
+
+	for address in IP.get_local_interfaces():
+		if address["friendly"] == "Wi-Fi" or address["friendly"].find("lan") != -1:
+			ip = address["addresses"].filter(func(x: String): return x.split(".").size() == 4)[0]
+			break
+	print(ip)
 	return ip
 
 func _on_name_text_changed():
